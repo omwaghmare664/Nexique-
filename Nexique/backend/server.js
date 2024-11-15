dc003947c0102f940e7e83885d5282a85cacd81b
Om Waghmare <164727053+CodingOm1@users.noreply.github.com>
2024-11-15 12:04:43 +0530

    Update server.js

--- Nexique/backend/server.js
+++ Nexique/backend/server.js
@@ -16,8 +16,8 @@ app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
 app.use(cors({
     origin: [
-        "https://nexique-o0pw.onrender.com",
-        "http://localhost:5174"
+        "https://nexique.onrender.com",
+        "https://nexique-admin.onrender.com"
     ], // specify your frontend origins as an array
     credentials: true // allow cookies and credentials
 }));
