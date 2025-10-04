fbcd52dff18d26091a0d9f61ae26c2e51fd155ea
CodingOm1 <164727053+CodingOm1@users.noreply.github.com>
2025-10-04 14:18:04 +0530

    landpage

--- Nexique/src/components/LandHome.jsx
+++ Nexique/src/components/LandHome.jsx
@@ -2,185 +2,23 @@ import React from "react";
 
 function LandHome() {
   return (
-    <div className="relative w-full h-screen flex justify-center items-center flex-col text-center gap-10 ">
-      <div className=" flex justify-center items-center flex-col font-Geo text-center gap-6">
-        <div className=" flex flex-col gap-5 text-2xl sm:text-3xl lg:text-5xl font-thin   heading  ">
-          Creative, Traditional Designs, <br />
-          Electronic Support.
-        </div>
-        <div className="flex items-center gap-5">
-          <input
-            type="text"
-            placeholder="Search Products"
-            className="w-[150px] h-5 p-3 outline-none border-[1px] border-[#8a8a8a71] sm:w-[200px] lg:w-[300px] sm:p-4 lg:p-5"
-          />
-          <button className="bg-[#000000] text-white py-[2px] px-6 sm:py-2 sm:px-8 lg:py-3 lg:px-10 rounded-lg">
-            Search
-          </button>
+    <div className="w-full min-h-screen relative ">
+
+      <div className="top-content w-full h-screen flex flex-col items-center justify-center bg-gradient-to-b from-[#fff0f0] to-white">
+        <div className="flex flex-col items-center justify-center w-full h-full absolute z-30 pointer-events-none">
+          <h1 className="text-[120px] font-bold text-[#ff7373]  landmaintextshadow bg-gradient-to-r from-[#ff7373] to-[#ff3c8a] text-transparent bg-clip-text animate-bounce-in">Want?</h1>
+          <h2 className="text-[31px]  font-medium -mt-5 ">A <span className="text-[#fc6363]">Powerful</span> <span className="text-[#6366f1] font-bold tracking-wide text-[36px]">AI Intergrated</span> Shopping!</h2>
+        </div>  
+        <div className="realview w-[55%] h-full absolute z-10 flex items-center justify-between ">
+          <div className="absolute top-[70%] left-[50%] setanchor z-20 w-[800px] h-[300px] bg-white blur-[60px]"></div>
+         
+    <div className="absolute left-[10%] w-[600px] h-[200px] bg-gradient-to-r from-pink-400 to-purple-400 rounded-full blur-3xl opacity-40 z-20"></div>
+
+          <img src="/testphone1.png" className="realimg1 hover:scale-105 hover:rotate-2 transition duration-500 w-60 rounded-[20px] shadow-2xl shadow-[#ff6b6b]/40" alt="" />
+          <img src="/testphone2.png" className="realimg2  -translate-y-25 hover:scale-105 hover:rotate-2 transition duration-500 w-60 rounded-[20px] shadow-2xl shadow-[#ff6b6b]/40" alt="" />
         </div>
       </div>
-      <div className="coverAttract">
-        <div className="shop-card">
-          <div className="slider">
-            <figure data-color="#e0d09a, #000000">
-              <img src="https://s7d5.scene7.com/is/image/Specialized/210164?$hero$" />
-            </figure>
-          </div>
-          <div className="title">Specialized Tarmac SL6</div>
-          <div className="desc">The New Tarmac</div>
 
-          <div className="cta">
-            <div className="price">rs.9.999</div>
-            <button className="btn">
-              Add to Cart<span className="bg"></span>
-            </button>
-          </div>
-        </div>
-        <div className="shop-card">
-          <div className="slider">
-            <figure data-color="#e0d09a, #000000">
-              <img src="https://img.freepik.com/free-photo/green-front-sweater_125540-736.jpg?t=st=1730046155~exp=1730049755~hmac=67b22de54239f48588e28a534da419a78a1cc743aa65787a920c56074b350ff3&w=740" />
-            </figure>
-          </div>
-          <div className="title">Specialized Tarmac SL6</div>
-          <div className="desc">The New Tarmac</div>
-
-          <div className="cta">
-            <div className="price">rs.9.999</div>
-            <button className="btn">
-              Add to Cart<span className="bg"></span>
-            </button>
-          </div>
-        </div>
-        <div className="shop-card">
-          <div className="slider">
-            <figure data-color="#e0d09a, #000000">
-              <img src="https://img.freepik.com/free-photo/t-shirt_1203-8005.jpg?t=st=1730046193~exp=1730049793~hmac=263d8ea3101688e31f337e9da807360eb07ec933a81d1f48f3ed4dbfa285e257&w=740" />
-            </figure>
-          </div>
-          <div className="title">Specialized Tarmac SL6</div>
-          <div className="desc">The New Tarmac</div>
-
-          <div className="cta">
-            <div className="price">rs.9.999</div>
-            <button className="btn">
-              Add to Cart<span className="bg"></span>
-            </button>
-          </div>
-        </div>
-        <div className="shop-card">
-          <div className="slider">
-            <figure data-color="#e0d09a, #000000">
-              <img src="https://img.freepik.com/free-psd/isolated-black-t-shirt-opened_125540-1283.jpg?t=st=1730046218~exp=1730049818~hmac=a2ff2f86e229322f6361a10fe9ba213d69af1af6799ee3f03f790915c6bbc35d&w=740" />
-            </figure>
-          </div>
-          <div className="title">Specialized Tarmac SL6</div>
-          <div className="desc">The New Tarmac</div>
-
-          <div className="cta">
-            <div className="price">rs.9.999</div>
-            <button className="btn">
-              Add to Cart<span className="bg"></span>
-            </button>
-          </div>
-        </div>
-        <div className="shop-card">
-          <div className="slider">
-            <figure data-color="#e0d09a, #000000">
-              <img src="https://img.freepik.com/free-photo/blue-t-shirt_125540-727.jpg?t=st=1730046239~exp=1730049839~hmac=80a57ec42fd22822154bb9d06b479da38ab7da2f52bbf8b385c6f112ce9aa525&w=740" />
-            </figure>
-          </div>
-          <div className="title">Specialized Tarmac SL6</div>
-          <div className="desc">The New Tarmac</div>
-
-          <div className="cta">
-            <div className="price">rs.9.999</div>
-            <button className="btn">
-              Add to Cart<span className="bg"></span>
-            </button>
-          </div>
-        </div>
-        <div className="shop-card">
-          <div className="slider">
-            <figure data-color="#e0d09a, #000000">
-              <img src="https://img.freepik.com/free-psd/t-shirt-print-concept_23-2147640180.jpg?t=st=1730046289~exp=1730049889~hmac=a95da36bc386b847fd2ecbfdafe36e5c049a91d0b19b3eb712e2a3d9fde65cb2&w=740" />
-            </figure>
-          </div>
-          <div className="title">Specialized Tarmac SL6</div>
-          <div className="desc">The New Tarmac</div>
-
-          <div className="cta">
-            <div className="price">rs.9.999</div>
-            <button className="btn">
-              Add to Cart<span className="bg"></span>
-            </button>
-          </div>
-        </div>
-        <div className="shop-card">
-          <div className="slider">
-            <figure data-color="#e0d09a, #000000">
-              <img src="https://img.freepik.com/premium-vector/ge-template-t-shirt_1172241-15861.jpg?w=740" />
-            </figure>
-          </div>
-          <div className="title">Specialized Tarmac SL6</div>
-          <div className="desc">The New Tarmac</div>
-
-          <div className="cta">
-            <div className="price">rs.9.999</div>
-            <button className="btn">
-              Add to Cart<span className="bg"></span>
-            </button>
-          </div>
-        </div>
-        <div className="shop-card">
-          <div className="slider">
-            <figure data-color="#e0d09a, #000000">
-              <img src="https://img.freepik.com/premium-photo/trendy-tshirt-design-vintage-typography-lettering-art-retro-slogan_992212-183384.jpg?w=740" />
-            </figure>
-          </div>
-          <div className="title">Specialized Tarmac SL6</div>
-          <div className="desc">The New Tarmac</div>
-
-          <div className="cta">
-            <div className="price">rs.9.999</div>
-            <button className="btn">
-              Add to Cart<span className="bg"></span>
-            </button>
-          </div>
-        </div>
-        <div className="shop-card">
-          <div className="slider">
-            <figure data-color="#e0d09a, #000000">
-              <img src="https://img.freepik.com/free-photo/graphic-tshirt-trendy-design-mockup-presented-wooden-hanger_460848-14039.jpg?t=st=1730046546~exp=1730050146~hmac=080dd436afc862b6adb11b6a2053d342e2c816c93b69a3d2a4f9e000cbe836d6&w=740" />
-            </figure>
-          </div>
-          <div className="title">Specialized Tarmac SL6</div>
-          <div className="desc">The New Tarmac</div>
-
-          <div className="cta">
-            <div className="price">rs.9.999</div>
-            <button className="btn">
-              Add to Cart<span className="bg"></span>
-            </button>
-          </div>
-        </div>
-        <div className="shop-card">
-          <div className="slider">
-            <figure data-color="#e0d09a, #000000">
-              <img src="https://img.freepik.com/free-photo/graphic-tshirt-trendy-design-mockup-presented-wooden-hanger_460848-14045.jpg?t=st=1730046557~exp=1730050157~hmac=e50aa44ce661d7c9e79556277a8fbaf63c690b5bbdd6034f3f68c29b8cc69e85&w=740" />
-            </figure>
-          </div>
-          <div className="title">Specialized Tarmac SL6</div>
-          <div className="desc">The New Tarmac</div>
-
-          <div className="cta">
-            <div className="price">rs.9.999</div>
-            <button className="btn">
-              Add to Cart<span className="bg"></span>
-            </button>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
